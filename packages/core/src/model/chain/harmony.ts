3cf6e7bf8904ead7ef0465d820b4e513dbf7b5ca
Sam Brasseale <sambrasseale@gmail.com>
2022-02-17 15:14:13 -0600

    updating changes to usedapp-harmony

--- packages/core/src/model/chain/harmony.ts
+++ packages/core/src/model/chain/harmony.ts
@@ -9,5 +9,13 @@ export const Harmony: Chain = {
   getExplorerAddressLink: (address: string) => `https://explorer.harmony.one/address/${address}`,
   getExplorerTransactionLink: (transactionHash: string) => `https://explorer.harmony.one/tx/${transactionHash}`,
 }
-
-export default { Harmony }
+export const HarmonyTestnet = {
+  chainId: 1666700000,
+  chainName: 'HarmonyTestnet',
+  isTestChain: true,
+  isLocalChain: false,
+  multicallAddress: '0xd078799c53396616844e2fa97f0dd2b4c145a685',
+  getExplorerAddressLink: (address: string) => `https://explorer.testnet.harmony.one/address/${address}`,
+  getExplorerTransactionLink: (transactionHash: string) => `https://explorer.testnet.harmony.one/tx/${transactionHash}`,
+};
+export default { Harmony, HarmonyTestnet }
